cefa009e75552990f8491e5c8080f93e754b9209
Stephen Searles <stephen.searles@shipwire.com>
2014-07-23 11:23:07 -0700

    Initial commit of a redis client library

---
+++ pool.go
@@ -0,0 +1,75 @@
+package redis
+
+import (
+	"sync"
+	"time"
+
+	"bitbucket.org/shipwire/redis/resp"
+)
+
+// Pool maintains a collection of idle Redis connections.
+type Pool struct {
+	pool *sync.Pool
+	idle int
+
+	Network, Server string
+
+	// Password indicates that new connections should initially authenticate
+	// with the given password. Password is ignored if empty.
+	Password string
+
+	// MaxIdle indicates how many idle connections should be kept. Zero indicates
+	// all idle connections should be preserved.
+	MaxIdle     int
+	ConnTimeout time.Duration
+}
+
+// NewPool intializes a connection pool with default settings.
+func NewPool() *Pool {
+	return &Pool{
+		pool: &sync.Pool{},
+		idle: 0,
+	}
+}
+
+func (p *Pool) put(c *Conn) error {
+	if p.MaxIdle != 0 && p.idle >= p.MaxIdle {
+		return c.Conn.Close()
+	}
+	p.pool.Put(c)
+	p.idle += 1
+	return nil
+}
+
+// Conn attempts to get or create a connection, depending on if there
+// are any idle connections.
+func (p *Pool) Conn() (*Conn, error) {
+	client := p.pool.Get().(*Conn)
+	if client == nil {
+		return p.newConn()
+	}
+	p.idle -= 1
+	return client, nil
+}
+
+func (p *Pool) newConn() (c *Conn, err error) {
+	if p.ConnTimeout != 0 {
+		c, err = DialTimeout(p.Network, p.Server, p.ConnTimeout)
+	} else {
+		c, err = Dial(p.Network, p.Server)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if p.Password != "" {
+		c.RawCmd("AUTH", p.Password)
+		if r := c.Resp(); r.Type() == resp.Error {
+			return nil, r.Error()
+		}
+	}
+
+	c.whence = p
+	return
+}
---
+++ resp/resp.go
@@ -0,0 +1,192 @@
+// Resp implements the REdis Serialization Protocol with the particular aim to communicate with
+// Redis. See http://redis.io/topics/protocol for more information.
+package resp
+
+import (
+	"bufio"
+	"errors"
+	"io"
+	"strconv"
+
+	"bitbucket.org/shipwire/forkedReader"
+)
+
+// RedisType represents one of the five types RESP values may take or it is unknown or invalid.
+type RedisType int
+
+const (
+	Unknown RedisType = iota
+	Invalid
+	SimpleString
+	Error
+	Integer
+	BulkString
+	Array
+	Null
+)
+
+var redisTypeMap map[string]RedisType = map[string]RedisType{
+	"+": SimpleString,
+	"-": Error,
+	":": Integer,
+	"$": BulkString,
+	"*": Array,
+}
+
+var (
+	InvalidResponse error = errors.New("invalid response")
+	InvalidType           = errors.New("wrong redis type requested")
+)
+
+// RESP holds an uninterpreted value in REdis Serialization Protocol. Values may be read
+// by the method for their correct type. While type can be checked as many times as the caller
+// wishes, each value may only be read once.
+type RESP struct {
+	r         *bufio.Reader
+	length    int64
+	redisType RedisType
+}
+
+// New creates a new RESP value from the given reader.
+func New(r io.Reader) *RESP {
+	return &RESP{
+		r: bufio.NewReader(r),
+	}
+}
+
+// Type determines the redis type of a RESP.
+func (r *RESP) Type() RedisType {
+	if r.redisType == Unknown {
+		firstByte := make([]byte, 1)
+		_, err := r.r.Read(firstByte)
+		if err != nil {
+			return Invalid
+		}
+
+		t := redisTypeMap[string(firstByte)]
+		switch t {
+		case BulkString, Array:
+			r.length, err = extractLength(r.r)
+			if r.length == -1 {
+				r.redisType = Null
+				break
+			}
+			fallthrough
+		case SimpleString, Integer, Error:
+			r.redisType = t
+		default:
+			r.redisType = Invalid
+		}
+	}
+	return r.redisType
+}
+
+func extractLength(r io.Reader) (int64, error) {
+	scanner := bufio.NewScanner(r)
+	if !scanner.Scan() {
+		return 0, scanner.Err()
+	}
+	return strconv.ParseInt(scanner.Text(), 10, 64)
+}
+
+// SimpleString returns the value of a RESP as a simple string
+func (r *RESP) SimpleString() (string, error) {
+	if r.Type() != SimpleString {
+		return "", InvalidType
+	}
+
+	scanner := bufio.NewScanner(r.r)
+	if !scanner.Scan() {
+		return "", scanner.Err()
+	}
+	return scanner.Text(), nil
+}
+
+// Error returns the value of a RESP as an error
+func (r *RESP) Error() error {
+	if r.Type() != Error {
+		return InvalidType
+	}
+	scanner := bufio.NewScanner(r.r)
+	if !scanner.Scan() {
+		return scanner.Err()
+	}
+	return errors.New(scanner.Text())
+}
+
+// Int returns the value of a RESP as an integer.
+func (r *RESP) Int() (int64, error) {
+	if r.Type() != Integer {
+		return 0, InvalidType
+	}
+	scanner := bufio.NewScanner(r.r)
+	if !scanner.Scan() {
+		return 0, scanner.Err()
+	}
+	return strconv.ParseInt(scanner.Text(), 10, 64)
+}
+
+// BulkString returns the value of a RESP as a reader.
+func (r *RESP) BulkString() (io.Reader, error) {
+	if r.Type() != BulkString {
+		return nil, InvalidType
+	}
+
+	head, tail := fork.ForkReader(r.r, int(r.length+2))
+	r.r = bufio.NewReader(tail)
+	return io.LimitReader(head, r.length), nil
+}
+
+// Array returns a channel on which callers can receive successive
+// elements of the RESP array.
+func (r *RESP) Array() (*RESPArray, error) {
+	if r.Type() != Array {
+		return nil, InvalidType
+	}
+
+	elements := make(chan *RESP)
+	array := &RESPArray{
+		length: int(r.length),
+		c:      elements,
+	}
+
+	go func() {
+		for err := (error)(nil); err == nil; {
+			elem := New(r.r)
+			elements <- elem
+		}
+		close(elements)
+	}()
+
+	return array, nil
+}
+
+// RESPArray contains a sequence of RESP items.
+type RESPArray struct {
+	length int
+	c      <-chan *RESP
+	cached []*RESP
+}
+
+// Next returns the next RESP item in the RESPArray.
+func (r *RESPArray) Next() *RESP {
+	if len(r.cached) > 0 {
+		ret := r.cached[0]
+		r.cached = r.cached[1:]
+		return ret
+	}
+	return <-r.c
+}
+
+// Cache reads the next RESP item and stores it in memory so subsequent items
+// may also be read.
+func (r *RESPArray) Cache() {
+	if elem := <-r.c; elem != nil {
+		r.cached = append(r.cached, elem)
+	}
+}
+
+// Len returns the total number of items in the RESPArray.
+func (r *RESPArray) Len() int {
+	return r.length
+}
